d6ea6bcf2cc6408bff11bf5dea2d91146956157e
Solaiman Hossain <slmnsnto@gmail.com>
2023-09-25 17:54:33 +0600

    add server.java file. and some changes in other files

--- src/Customer.java
+++ src/Customer.java
@@ -1,17 +1,61 @@
 import java.util.Comparator;
 
 public class Customer {
-    int arrivalTime;
+    private int arrivalTime;
     private int departureTime;
-    int executionTime;
+    private int startTime;
+    private int executionTime;
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(int departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(int executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
     private int waitingTime;
 
-    public Customer(){
+    public Customer(int arrivalTime, int executionTime) {
+        this.arrivalTime = arrivalTime;
+        this.executionTime = executionTime;
     }
 }
 class MyComparator implements Comparator<Customer> {
     @Override
     public int compare(Customer c, Customer c2) {
-        return Integer.compare(c.arrivalTime, c2.arrivalTime);
+        return Integer.compare(c.getArrivalTime(), c2.getArrivalTime());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,27 @@ public class Main  {
     private static final List<Customer> customer = new ArrayList<>();
 
     public Main(){
+
     }
 
     public static void main(String[] args) {
         int n = random.nextInt(20);
         for(int i=0; i<n; i++){
-            customer.add(new Customer());
-            customer.get(i).arrivalTime = random.nextInt(10)+1;
-            customer.get(i).executionTime = random.nextInt(10)+1;
+            customer.add(new Customer(random.nextInt(10)+1, random.nextInt(10)+1) );
         }
         customer.sort(new MyComparator());
+
+        int prev = 0;
         for(int i=0; i<n; i++){
-            System.out.println(customer.get(i).arrivalTime + " " + customer.get(i).executionTime);
+            customer.get(i).setStartTime( Math.max(prev, customer.get(i).getArrivalTime()));
+            customer.get(i).setDepartureTime( customer.get(i).getStartTime() + customer.get(i).getExecutionTime());
+            customer.get(i).setWaitingTime(customer.get(i).getStartTime() - customer.get(i).getArrivalTime());
+            prev = customer.get(i).getDepartureTime();
+        }
+        for(int i=0; i<n; i++){
+            System.out.println(i+1 + " arrival time: " + customer.get(i).getArrivalTime() + " waiting time: " +
+                    customer.get(i).getWaitingTime() + " start time " + customer.get(i).getStartTime() + " execution time " +
+                    customer.get(i).getExecutionTime() + " departure time " + customer.get(i).getDepartureTime());
         }
     }
 }
